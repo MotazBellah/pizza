f1a823000165fe02b3f339c146f1c1cc86289faa
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-04-13 03:15:07 +0200

    Created register, login and logout route

--- restaurant/pizza/urls.py
+++ restaurant/pizza/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index),
+    path("", views.index, name='index'),
+    path("register", views.register, name='register'),
+    path("signin", views.signin, name='signin'),
+    path("signout", views.signout, name='signout'),
 ]
--- restaurant/pizza/views.py
+++ restaurant/pizza/views.py
@@ -1,4 +1,7 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
+from django.urls import reverse
 from django.shortcuts import render
 from .models import Size, Topping
 
@@ -15,13 +18,44 @@ def index(request):
         else:
             menu[i[0]] = [i[1:]]
 
-    # print(menu['Regular Pizza'])
-    # print(menu['Sicilian Pizza'])
-    for i in menu.items():
-        print(i)
     context = {
         'items': menu.items(),
         'toppings': Topping.objects.all()
     }
-
     return render(request, "pizza/index.html", context)
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        password2 = request.POST["password2"]
+        if not password == password2:
+            return render(request, "pizza/register.html", {"message":"Passwords don't match."})
+
+        user = User.objects.create_user(username, email, password)
+        user.username = username
+        user.email = email
+        user.save()
+
+        return render(request, "pizza/login.html", {"message":"Registered. You can log in now."})
+    return render(request, "pizza/register.html")
+
+
+def signin(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "pizza/login.html", {"message": "Invalid credentials."})
+    return render(request, "pizza/login.html")
+
+
+def signout(request):
+    logout(request)
+    return render(request, "pizza/login.html", {"message": "Logged out."})
