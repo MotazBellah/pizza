cd22adc1acf17ff6e12052ff02859d9a2a8e04c0
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-05-27 07:14:33 +0200

    Added more comments to discribe the code, Edite some name variable to be more clear and Delete console.log

--- restaurant/pizza/static/app.js
+++ restaurant/pizza/static/app.js
@@ -5,40 +5,42 @@ var items = '';
 
 
 menu.addEventListener('click', (e) => {
-    // selectionLimit(2, 'test');
+    // Call the selectionLimit function and set the limit based on the topping
     selectionLimit(2, 'tw');
     selectionLimit(3, 'th');
     selectionLimit(2, 'two-item');
     selectionLimit(3, 'three-item');
     selectionLimit(5, 'special');
 
+    // If the input is hitted, use DOM traverse to get the price, food, typeID and toppings
+    // and submit the form with AJAX
     if (e.target.tagName === 'INPUT') {
-        var form = e.target.parentNode.parentNode.id;
-        var price = e.target.value
-        var food = e.target.parentNode.parentNode.childNodes[3].value
-        var typeID = e.target.parentNode.parentNode.childNodes[3].id;
-        console.log(e.target.parentNode.parentNode.childNodes);
+        const form = e.target.parentNode.parentNode.id
+        const price = e.target.value
+        const food = e.target.parentNode.parentNode.childNodes[3].value
+        const typeID = e.target.parentNode.parentNode.childNodes[3].id
         const select1 = e.target.parentNode.parentNode.childNodes[5]
+        const check = true
+        const id = "#" + form
 
-        var selected1 = []
+        // If the user select topping, then
+        // Create list with all selected options
+        let selected1 = []
         if (select1) {
-            for (var i = 0; i < select1.length; i++) {
+            for (let i = 0; i < select1.length; i++) {
             if (select1.options[i].selected) {
                 selected1.push(select1.options[i].value);
             }
         }
 
         }
-        console.log(typeID);
-        console.log(selected1);
-        console.log(price);
-        console.log(food);
-        var check = true
-        var id = "#" + form
 
-        console.log("#" + form);
         $(document).on('submit', id, function(e) {
+            // preventDefault, do not allow to write the parameter on the url
             e.preventDefault();
+            // Make sure the number of selected topping equal to the selected food
+            // For ex. if the user select 2 toppings then the user has two options to select not LESS
+            // If user select less than the selected topping, informe the user
             if (check) {
                 if ((food=='2 toppings'||food=='2 items') && selected1.length < 2) {
                     alert("Please select 2 toppings")
@@ -55,6 +57,7 @@ menu.addEventListener('click', (e) => {
                     check = false;
                     return
                 }
+                // Created AJAX request to send the info about the selected food to the server
                 $.ajax({
                     type: 'post',
                     url: '/addFood',
@@ -81,11 +84,13 @@ menu.addEventListener('click', (e) => {
 
 });
 
+// Function to limit the number of topping selection
+// If the user for example select topping 2, so he has only two options to select not MORE
+// If the user select more than the limit, set the select value to be null and display a message
 function selectionLimit(limit, id) {
-    var el = document.getElementById(id);
+    const el = document.getElementById(id);
     if (el) {
         el.addEventListener("change", function() {
-            // console.log(e.target.value);
             if ($(this).val().length > limit) {
                 $(this).val(null);
                 alert('You can select upto ' + limit.toString() +' options only');
