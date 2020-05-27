3893c4dfd2df636a5e249f2507d5630ac23ccad3
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-05-27 05:38:18 +0200

    Added comment

--- restaurant/restaurant/settings.py
+++ restaurant/restaurant/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -88,7 +89,7 @@ DATABASES = {
     }
 }
 
-import dj_database_url
+# Config the databases, and let the app use heroku 'env' DB
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
